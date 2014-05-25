7bedccc77d4eb8fecf9d5107ea032d4ae57f0afd
ned rihine <ned.rihine@gmail.com>
2014-05-25 13:01:00 +0900

    uni 用の型を typedef した

--- include/uni/urchin.h
+++ include/uni/urchin.h
@@ -1,5 +1,30 @@
 #ifndef uni_urchin_h
 #define uni_urchin_h
 
+typedef    unsigned char     URBoolean;
+
+typedef    signed char       URSInt8;
+typedef    signed short      URSInt16;
+typedef    signed long       URSInt32;
+typedef    signed int        URSInt;
+
+typedef    unsigned char     URUInt8;
+typedef    unsigned short    URUInt16;
+typedef    unsigned long     URUInt32;
+typedef    unsigned int      URUInt;
+
+typedef    void*             URValue;
+
+typedef    void      (*URAction)(void);
+
+typedef    URBoolean (*URValuePredicate)(URValue left, URValue right);
+
+
+#ifndef FALSE
+#   define    FALSE   ((URBoolean)0)
+#endif  /* ndef TRUE */
+#ifndef TRUE
+#   define    TRUE    ((URBoolean)1)
+#endif  /* ndef TRUE */
 
 #endif  /* uni_urchin_h */
